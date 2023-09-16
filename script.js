9c94a163979111e1d13376affca158afe51b5c0b
Fuentes11 <peke3025@gmail.com>
2023-09-15 22:48:01 -0600

    changes for month

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const tabLinks = document.querySelectorAll(".custom-nav-tabs .nav-link");
     const tabContents = document.querySelectorAll(".tab-pane");
+    const uploadButton = document.getElementById("uploadButton");
+    const uploadedImage = document.getElementById("uploadedImage");
 
-    tabLinks.forEach(link => {
-        link.addEventListener("click", function(e) {
+    // Comprueba si hay una imagen guardada en localStorage
+    const savedImageUrl = localStorage.getItem("uploadedImage");
+    if (savedImageUrl) {
+        uploadedImage.src = savedImageUrl;
+    }
+
+    tabLinks.forEach((link) => {
+        link.addEventListener("click", function (e) {
             e.preventDefault();
 
-            tabContents.forEach(content => {
+            tabContents.forEach((content) => {
                 content.classList.remove("active");
             });
 
             const targetTab = this.getAttribute("data-bs-target");
             document.querySelector(targetTab).classList.add("active");
 
-            tabLinks.forEach(link => {
+            tabLinks.forEach((link) => {
                 link.classList.remove("active");
             });
 
             this.classList.add("active");
         });
+    });
+
+    // Agregar un evento de cambio al botón de carga
+    uploadButton.addEventListener("change", function () {
+        const file = uploadButton.files[0];
+
+        if (file) {
+            const imageUrl = URL.createObjectURL(file);
+            uploadedImage.src = imageUrl;
+
+            // Guardar la URL de la imagen en localStorage
+            localStorage.setItem("uploadedImage", imageUrl);
+
+            uploadedImage.onload = function () {
+                URL.revokeObjectURL(imageUrl);
+            };
+        }
     });
 });
